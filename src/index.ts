3b29344679ce54d94c64892f45e1708321dd7488
bipanshu <bipanshukumar21@gmail.com>
2026-01-17 15:47:10 +0530

    added skeleton for middleware, routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
 import http from "http"
- 
+
+type HTTPmethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE"
+
+interface Request extends http.IncomingMessage{
+
+}
+
+interface Response extends http.ServerResponse{
+
+}
+
+type Handler = (req : Request, res : Response) => void | Promise<void>;
+type MiddleWare = (req : Request, res :Response, next : ()=> void) => void | Promise<void>;
+
+interface Route{
+
+}
+
+class Router{
+
+}
+
+function jsonParser() : MiddleWare{
+
+}
+
+function logger() : MiddleWare{
+    
+}
+
+const router = new Router()
 
 const httpServer = http.createServer((req , res )=>{
 
