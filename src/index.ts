0cc8db88a365045b772bf46d3972f736f6ce8a51
bipanshu <bipanshukumar21@gmail.com>
2026-01-17 16:11:15 +0530

    req, res interface done

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,25 @@ import http from "http"
 type HTTPmethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE"
 
 interface Request extends http.IncomingMessage{
-
+    body? : any;
+    params? : Record<string, string>;
+    query? : Record<string, string>;
 }
 
 interface Response extends http.ServerResponse{
-
+    json : (data : any) => void;
+    status : (code : number) => Response; //doubt here that why Response
+    send : (data : string) => void;
 }
 
-type Handler = (req : Request, res : Response) => void | Promise<void>;
+type Handler = (req : Request, res : Response) => void | Promise<void>; //doubt about Promise
 type MiddleWare = (req : Request, res :Response, next : ()=> void) => void | Promise<void>;
 
 interface Route{
-
+    method : HTTPmethod,
+    pattern : RegExp,
+    paramNames : string[],
+    
 }
 
 class Router{
@@ -26,7 +33,7 @@ function jsonParser() : MiddleWare{
 }
 
 function logger() : MiddleWare{
-    
+
 }
 
 const router = new Router()
