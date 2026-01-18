0a548bcb724b43aba198a27b73f46f48f5014223
bipanshu <bipanshukumar21@gmail.com>
2026-01-18 14:39:38 +0530

    implementedd regex, queryparse, logger & jsonparser middleware

--- src/index.ts
+++ src/index.ts
@@ -2,44 +2,142 @@ import http from "http"
 
 type HTTPmethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE"
 
-interface Request extends http.IncomingMessage{
-    body? : any;
-    params? : Record<string, string>;
-    query? : Record<string, string>;
+interface Request extends http.IncomingMessage {
+    body?: any;
+    params?: Record<string, string>;
+    query?: Record<string, string>;
 }
 
-interface Response extends http.ServerResponse{
-    json : (data : any) => void;
-    status : (code : number) => Response; //doubt here that why Response
-    send : (data : string) => void;
+interface Response extends http.ServerResponse {
+    json: (data: any) => void;
+    status: (code: number) => Response;
+    send: (data: string) => void;
 }
 
-type Handler = (req : Request, res : Response) => void | Promise<void>; //doubt about Promise
-type MiddleWare = (req : Request, res :Response, next : ()=> void) => void | Promise<void>;
+type Handler = (req: Request, res: Response) => void | Promise<void>;
+type MiddleWare = (req: Request, res: Response, next: () => void) => void | Promise<void>;
 
-interface Route{
-    method : HTTPmethod,
-    pattern : RegExp,
-    paramNames : string[],
+interface Route {
+    method: HTTPmethod,
+    pattern: RegExp,
+    paramNames: string[],
+    handler: Handler
+}
+
+class Router {
+    private routes: Route[] = [];
+    private middlewares: MiddleWare[] = [];
+
+    use(middleware: MiddleWare) {
+        this.middlewares.push(middleware)
+    }
+
+    private addRoute(method: HTTPmethod, path: String, handler: Handler) {
+        const { pattern, paramNames } = this.pathToRegex(path)
+        this.routes.push({ method, pattern, paramNames, handler })
+    }
+
+    get(path: String, handler: Handler) {
+        this.addRoute("GET", path, handler)
+    }
+
+    post(path: String, handler: Handler) {
+        this.addRoute("POST", path, handler)
+    }
+
+    put(path: String, handler: Handler) {
+        this.addRoute("PUT", path, handler)
+    }
+
+    delete(path: String, handler: Handler) {
+        this.addRoute("DELETE", path, handler)
+    }
+
+    patch(path: String, handler: Handler) {
+        this.addRoute("PATCH", path, handler)
+    }
+
+    private pathToRegex(path: String): { pattern: RegExp, paramNames: string[] } {
+        const _paramNames: string[] = []
+        const pattern = path
+            .replace(/\//g, "\\/")
+            .replace(/:([^\/]+)/g, (_, paramName) => {
+                _paramNames.push(paramName);
+                return "([^\\/]+)";
+            });
+
+            return { pattern: new RegExp(`^${pattern}$`), paramNames: _paramNames };    
+    }   
     
+    private parseQuery(url : String) : Record <string, string>{
+        const queryString = url.split("?")[1]
+        if(!queryString) return {}
+
+        return queryString.split("&").reduce((acc, pair)=>{
+            const [key, value] = pair.split("=")
+            acc[decodeURIComponent(key)] = decodeURIComponent(value || " ")
+            return acc
+        }, {} as Record <string, string>);
+    } 
+
+    async handle(req : Request, res : Response){
+        res.json =  (data : any) =>{
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(data))
+        }
+
+        res.status = (code : number) =>{
+            res.statusCode = code;
+            return res;
+        }
+
+        res.send = (data : string) =>{
+            res.end(data);
+        };
+    }
 }
 
-class Router{
-
+function jsonParser(): MiddleWare {
+    return (req, res, next) => {
+        if (req.method === "POST" || "PUT" || "PATCH") {
+            let body = ""
+            req.on("data", (chunk) => {
+                body += chunk.toString()
+            })
+            req.on("end", () => {
+                try{
+                    req.body = body ? JSON.parse(body) : {}
+                }catch(err){
+                    req.body = {}
+                }
+            })
+            next()
+        }else{
+            next()
+        }
+    };
 }
 
-function jsonParser() : MiddleWare{
-
-}
-
-function logger() : MiddleWare{
-
+function logger(): MiddleWare {
+    return (req, res, next) => {
+       
+    } 
 }
 
 const router = new Router()
+router.use(logger())
+router.use(jsonParser())
 
-const httpServer = http.createServer((req , res )=>{
-
+const httpServer = http.createServer((req, res) => {
+    router.handle(req as Request, res as Response)
 })
 
-httpServer.listen(8080, ()=>console.log(`server listening at ${8080}`))
+const PORT = 8080
+httpServer.listen(PORT, ()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log("\nAvailable routes:");
+    console.log("  GET  /");
+    console.log("  GET  /users/:id");
+    console.log("  POST /users");
+    console.log("  GET  /status");
+})
